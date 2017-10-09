3b76931d5cb01c49535950fdad8f02ce1158e88d
Eric Londaits <eric.londaits@gmail.com>
2017-10-09 09:26:19 -0300

    Changes the order of JS behaviors initialization to fix TOC marker not appearing on first load.

--- src/js/main.js
+++ src/js/main.js
@@ -4,42 +4,6 @@ const SlideTransitionDelay = 500;
 
 $(() => {
   /**
-   * Installs a router that slides the slideshow to the offset based on the url hash
-   *
-   * The route handler also fires events on the slides:
-   * - slideEnter:
-   *   Fired when a new slide is selected, before the slideshow finishes transitioning to it.
-   * - slideEntered:
-   *   Fired after the slideshow finishes transitioning to the new slide.
-   * - slideExit:
-   *   Fired when a slide is being exited, before the slideshow begins transitioning away.
-   * - slideExited:
-   *   Fired when the slideshow finished transitioning away from an exited slide.
-   *
-   * NOTE: It's currently possible that a slideEnter event will be fired on a slide before the
-   * slideExited handler of the same slide finished firing. Same for slideExit / slideEntered.
-   */
-  $('.slideshow').each((iSlideshow, slideshow) => {
-    let currentSlide = null;
-    $(slideshow).find('[data-slide-id]').each((iSlide, slide) => {
-      routie($(slide).attr('data-slide-id'), () => {
-        if (currentSlide !== null) {
-          $(currentSlide).trigger('slideExit');
-          window.setTimeout((exitedSlide) => {
-            $(exitedSlide).trigger('slideExited');
-          }, SlideTransitionDelay, currentSlide);
-        }
-        currentSlide = slide;
-        $(slide).trigger('slideEnter');
-        setTimeout((enteredSlide) => {
-          $(enteredSlide).trigger('slideEntered');
-        }, SlideTransitionDelay, slide);
-        $(slideshow).css({ marginLeft: -1 * $(slide).position().left });
-      });
-    });
-  });
-
-  /**
    * Install toggler controls
    */
   function showOption(slide, optionID) {
@@ -151,6 +115,42 @@ $(() => {
         } else {
           stopRequested = true;
         }
+      });
+    });
+  });
+
+  /**
+   * Installs a router that slides the slideshow to the offset based on the url hash
+   *
+   * The route handler also fires events on the slides:
+   * - slideEnter:
+   *   Fired when a new slide is selected, before the slideshow finishes transitioning to it.
+   * - slideEntered:
+   *   Fired after the slideshow finishes transitioning to the new slide.
+   * - slideExit:
+   *   Fired when a slide is being exited, before the slideshow begins transitioning away.
+   * - slideExited:
+   *   Fired when the slideshow finished transitioning away from an exited slide.
+   *
+   * NOTE: It's currently possible that a slideEnter event will be fired on a slide before the
+   * slideExited handler of the same slide finished firing. Same for slideExit / slideEntered.
+   */
+  $('.slideshow').each((iSlideshow, slideshow) => {
+    let currentSlide = null;
+    $(slideshow).find('[data-slide-id]').each((iSlide, slide) => {
+      routie($(slide).attr('data-slide-id'), () => {
+        if (currentSlide !== null) {
+          $(currentSlide).trigger('slideExit');
+          window.setTimeout((exitedSlide) => {
+            $(exitedSlide).trigger('slideExited');
+          }, SlideTransitionDelay, currentSlide);
+        }
+        currentSlide = slide;
+        $(slide).trigger('slideEnter');
+        setTimeout((enteredSlide) => {
+          $(enteredSlide).trigger('slideEntered');
+        }, SlideTransitionDelay, slide);
+        $(slideshow).css({ marginLeft: -1 * $(slide).position().left });
       });
     });
   });
