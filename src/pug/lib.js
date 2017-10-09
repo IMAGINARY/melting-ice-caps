2d6885a78d0e54c3708aaa919d1dffd327191a8a
Eric Londaits <eric.londaits@gmail.com>
2017-10-09 00:11:35 -0300

    English content.

--- src/pug/lib.js
+++ src/pug/lib.js
@@ -1,3 +1,19 @@
-module.exports = {
+var lang = 'en';
 
+function getLang() {
+  return lang;
+}
+
+function setLang(langCode) {
+  lang = langCode;
+}
+
+function localImage(name) {
+  return 'assets/img/' + lang + '/' + name;
+}
+
+module.exports = {
+  setLang: setLang,
+  getLang: getLang,
+  localImage: localImage
 };
